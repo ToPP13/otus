570e46ddbf0c0eb5f2791f954e51c82fe4664b99
Kirill <topp13@mail.ru>
2020-01-20 22:44:19 +0300

    Correct main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include "version.h"
+#include "lib.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World! " << endl;
-    cout << "Version: " << PROJECT_VERSION_PATCH << endl;
+    cout << "Version: " << version() << endl;
     return 0;
 }

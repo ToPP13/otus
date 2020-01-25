e46ed9de16f80cbb51fdc532f9904242673ae577
Kirill <topp13@mail.ru>
2020-01-25 21:17:25 +0300

    Try to add tast_1 subdirectory

---
+++ task_1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "lib.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World! " << endl;
+    cout << "Version: " << version() << endl;
+    return 0;
+}

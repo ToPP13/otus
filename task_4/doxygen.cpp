d42b976e4bac16fa2244bf285293eb52cab95be8
Kirill <topp13@mail.ru>
2020-03-09 14:07:43 +0300

    Test build task_4

---
+++ task_4/doxygen.cpp
@@ -0,0 +1,16 @@
+#ifndef __PRETTY_FUNCTION__
+#include "pretty.h"
+#endif
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+#include "lib.h"
+
+
+
+
+int main(int, char *[]) {
+    return 0;
+}
--- task_4/lib.cpp
+++ task_4/lib.cpp
@@ -7,12 +7,3 @@ int version()
     return PROJECT_VERSION_PATCH;
 }
 
-long int fact(int N)
-{
-    if(N < 0)
-        return 0;
-    if (N == 0)
-        return 1;
-    else
-        return N * fact(N - 1);
-}
